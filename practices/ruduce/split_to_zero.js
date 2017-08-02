e946a8f38b4b28ce204b89fa94598dbf71e54acf
Summer-z <979971754@qq.com>
2017-08-02 10:19:13 +0000

    second modified

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,11 +2,11 @@
 
 function spilt_to_zero(number, interval) {
           let result = [];
-          
-          while(!(number===0)||(number<0)) {
-                   result.push(number); 
-                   number = parseFloat(number - interval).toFixed(1);
-          }
+//          
+//          while(!(number===0)||(number<0)) {
+//                   result.push(number); 
+//                   number = parseFloat(number - interval).toFixed(1);
+//          }
           return result;
 }
 

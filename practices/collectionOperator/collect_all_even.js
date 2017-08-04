64d3bd3be858fcc78725217ccfa557fedbf60e02
Summer-z <979971754@qq.com>
2017-08-04 10:11:53 +0800

    test

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -4,21 +4,20 @@ function even(item) {
           if (item%2 === 0) {
                     return true;
           }
-          
+
           return false;
 }
 
 function collect_all_even(collection) {
           let evenArr = [];
-          
-          for (let item of collection) { 
+
+          for (let item of collection) {
                    if (even(item)) {
                              evenArr.push(item);
-                   }    
+                   }
           }
-          
+
           return evenArr;
-          
 }
 
 module.exports = collect_all_even;
